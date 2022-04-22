72e1bc36f22639c56200f0d06632943cfc005b4b
martynhorslen <2880060+MartynHorslen@users.noreply.github.com>
2022-04-22 15:58:16 +0100

    Add menu animation for smaller view widths.

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+$('nav > a').on("click", () => {
+    if ($('header nav ul').css("display") === "none"){
+        $('header nav ul').slideDown(600).css("display", "flex");
+    } else {
+        $('header nav ul').slideUp(600);
+    }
+});
+$( window ).resize(() => {
+    if (window.innerWidth >= 768){
+        $('header nav ul').slideDown(600).css("display", "flex");
+    } else {
+        $('header nav ul').hide();
+    }
+});
