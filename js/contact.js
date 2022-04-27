2f3462173319d610100bcabdecf30f19de59ec7a
martynhorslen <2880060+MartynHorslen@users.noreply.github.com>
2022-04-27 14:21:30 +0100

    Add text area validation

--- js/contact.js
+++ js/contact.js
@@ -1,36 +1,72 @@
+//If JavaScript is active, remove the labels.
 $("label").addClass('hide-label');
+
+//Turn off rexex pattern validation as this will now be handled by JavaScript.
 $("#contact-form form").attr("novalidate", "");
 
+//Regex patterns
 const test = ["^[a-zA-Z-]{3,16}$", "^[a-zA-Z-]{3,16}$", "^([a-zA-Z0-9_\.\+-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$", "[A-Za-z0-9\W]{4,80}", "[A-Za-z0-9\W]+"];
 
+//Regex test function
 const regexTest = (value, test) => {
     let regex = new RegExp (test);
     return regex.test(value);
 }
 
+//Event handler for clicking submit button
 $("#contact-form button").on("click", (e) => {
+    //Prevent form from reloading the page.
     e.preventDefault();
+
+    //Count for valid form fields
     let valid = 0;
+
+    // For each input field...
     $("#contact-form input").each((i, element) => {
+        //...get the value
         let val = $(element).val();
         
+        // Run the regex test
         if (!regexTest(val, test[i])){
+            //If test fails, show the label and add a border to the element.
             $("label").eq(i).removeClass('hide-label').css("color", "red");
             $(element).css("border", "2px solid red").css("color", "red");
-            console.log(`Form input ${i} failed Regex.`);
         } else {
-            console.log('Data validated.');
+            // If test passes, hide the label.
             $("label").eq(i).addClass('hide-label');
             valid++;
         }
+
+        //Remove the element styling above once user edits the input so that the stylesheet handles valid/invalid styling again.
         $(element).on('input', () => {
             $(element).attr("style", "none");
         });
     });
-    
-    if (valid === 4) {
+    // For the textarea field...
+    $("#contact-form textarea").each((i, element) => {
+        //...get the value
+        let val = $(element).val()
+        
+        // Run the regex test for message textarea.
+        if (!regexTest(val, test[4])){
+            //If test fails, show the label and add a border to the element.
+            $("label").eq(4).removeClass('hide-label').css("color", "red");
+            $(element).css("border", "2px solid red").css("color", "red");
+        } else {
+            // If test passes, hide the label.
+            $("label").eq(4).addClass('hide-label');
+            valid++;
+        }
+
+        //Remove the element styling above once user edits the input so that the stylesheet handles valid/invalid styling again.
+        $(element).on('input', () => {
+            $(element).attr("style", "none");
+        });
+    });
+    //If all inputs are valid...
+    if (valid === 5) {
         console.log("All imputs valid. Process form.");
     } else {
-        console.log(`${valid}/4 inputs valid.`);
+        console.log(`${valid}/5 inputs valid.`);
     }
 })
