78e48ead68848f25434a8ead8edf4bcd9fe3611a
martynhorslen <2880060+MartynHorslen@users.noreply.github.com>
2022-05-11 18:31:35 +0100

    Added smooth scrolling and fixed typing effect bug

--- js/type-effect.js
+++ js/type-effect.js
@@ -1,28 +1,31 @@
 var i = 0;
 var txt = $('#type-effect').text(); /* The text */
 var speed = 100; /* The speed/duration of the effect in milliseconds */
-let running = false;
+let running = true;
 
 function typeWriter() {
-  running = true;
-  if (i < txt.length) {
-    document.getElementById("type-effect").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  } else {
-    running = false;
-  }
-  return;
+    running = true;
+    if (i < txt.length) {
+      document.getElementById("type-effect").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    } else {
+      running = false;
+    }
+    return;
 }
+
 $(document).ready(()=>{
+    running = false;
     $('#type-effect').text("");
     typeWriter();
 });
+
 $('figcaption').on('mouseenter click', () => {
-    if (running === false){
-      $('#type-effect').text("");
-      i=0;
-      typeWriter();
-    }
+  if (running === false){
+    $('#type-effect').text("");
+    i=0;
+    typeWriter();
+  }
 });
 
